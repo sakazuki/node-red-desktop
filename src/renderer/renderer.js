c8504f5b6f2c94cf287ae6350193f2f42fe4b661
sakazuki <sakazuki@gmail.com>
2019-11-10 12:00:38 +0900

    fix bugs

--- src/renderer/renderer.js
+++ src/renderer/renderer.js
@@ -3,73 +3,75 @@
 const electron = window.nodeRequire("electron");
 const ipc = electron.ipcRenderer;
 
-$("#full-shade").after('<div id="nrd-shade" class="hide"></div>');
+$(document).ready(function(){
+  $("#red-ui-full-shade").after('<div id="nrd-shade" class="hide"></div>');
 
-RED.events.on("nodes:change",function(state) {
-  ipc.send("nodes:change", state);
-});
-
-RED.events.on("view:selection-changed", function(selection) {
-  ipc.send("view:selection-changed", selection);
-})
-
-ipc.on("force:reload", function() {
-  window.onbeforeunload = null;
-  $("#btn-deploy").addClass("disabled");
-})
-
-ipc.on("editor:deploy",  (event, message) => {
-  $("#btn-deploy").click();
-});
-
-ipc.on("editor:start", (event, message) => {
-  var observer  = new MutationObserver((mutationRecords, observer) => {
-    var target = $("#red-ui-tab-debug a");
-    if (target.length){
-      observer.disconnect();
-      target.click();
-      ipc.send("editor:started");
-    }
+  RED.events.on("nodes:change",function(state) {
+    ipc.send("nodes:change", state);
   });
-  observer.observe(document, {childList:true, subtree: true});
-});
 
-ipc.on("shade:show", (event, message) => {
-  $("#nrd-shade").show();
-});
+  RED.events.on("view:selection-changed", function(selection) {
+    ipc.send("view:selection-changed", selection);
+  })
 
-ipc.on("shade:start", (event, message) => {
-  $("#nrd-shade").css("background-image", "url(red/images/spin.svg)");
-});
+  ipc.on("force:reload", function() {
+    window.onbeforeunload = null;
+    $("#red-ui-header-button-deploy").addClass("disabled");
+  })
 
-ipc.on("shade:end", (event, message) => {
-  $("#nrd-shade").css("background-image", "none");
-});
+  ipc.on("editor:deploy",  (event, message) => {
+    $("#red-ui-header-button-deploy").click();
+  });
 
-ipc.on("shade:hide", (event, message) => {
-  $("#nrd-shade").css("background-image", "none");
-  $("#nrd-shade").hide();
-});
-
-ipc.on("red:notify", (event, type, message, timeout) => {
-  var options = {
-    type: type,
-    modal: true,
-    fixed: false,
-    timeout: timeout
-  };
-  if (!timeout) {
-    Object.assign(options, {
-      fixed: true,
-      buttons: [
-        {
-          text: RED._("common.label.close"),
-          click: function() {
-            notification.close();
-          }
-        }
-      ]
+  ipc.on("editor:start", (event, message) => {
+    var observer  = new MutationObserver((mutationRecords, observer) => {
+      var target = $("#red-ui-tab-debug-link-button");
+      if (target.length){
+        observer.disconnect();
+        target.click();
+        ipc.send("editor:started");
+      }
     });
-  }
-  var notification = RED.notify(message, options);
+    observer.observe(document, {childList:true, subtree: true});
+  });
+
+  ipc.on("shade:show", (event, message) => {
+    $("#nrd-shade").show();
+  });
+
+  ipc.on("shade:start", (event, message) => {
+    $("#nrd-shade").css("background-image", "url(red/images/spin.svg)");
+  });
+
+  ipc.on("shade:end", (event, message) => {
+    $("#nrd-shade").css("background-image", "none");
+  });
+
+  ipc.on("shade:hide", (event, message) => {
+    $("#nrd-shade").css("background-image", "none");
+    $("#nrd-shade").hide();
+  });
+
+  ipc.on("red:notify", (event, type, message, timeout) => {
+    var options = {
+      type: type,
+      modal: true,
+      fixed: false,
+      timeout: timeout
+    };
+    if (!timeout) {
+      Object.assign(options, {
+        fixed: true,
+        buttons: [
+          {
+            text: RED._("common.label.close"),
+            click: function() {
+              notification.close();
+            }
+          }
+        ]
+      });
+    }
+    var notification = RED.notify(message, options);
+  });
 });
