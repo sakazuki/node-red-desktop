18b23d47cf94a5b7123c005ed164eb09cfa5e56e
sakazuki <sakazuki@gmail.com>
2021-08-11 17:41:18 +0900

    wip

--- build.js
+++ build.js
@@ -6,36 +6,55 @@ const config = yaml.load(fs.readFileSync("./electron-builder.yml"));
 const Platform = builder.Platform;
 const program = require("commander");
 
-program.option("-n --no-build").parse(process.argv);
+async function copyFiles() {
+  const files = [
+    "package.json",
+    "loading.html",
+    "settings.html",
+    "images",
+    "locales",
+    "src/renderer/desktop.css"
+  ];
+  for (let file of files) {
+    await fs.copy(file, path.join(__dirname, config.directories.app, file.replace("src/", "")));
+  }
+  return files;
+}
 
+async function patchFiles() {
+  // issue#49 https://github.com/sakazuki/node-red-desktop/issues/49
+  await fs.copy('patch/i18n.js', path.join(__dirname, config.directories.app, "node_modules/@node-red/util/lib/i18n.js"));
+  return
+}
 
-(async () => {
+async function build() {
+  const platform = (process.platform === "darwin") ? Platform.MAC : Platform.WINDOWS;
+  return await builder.build({
+    targets: platform.createTarget(),
+    config
+  });
+}
+
+async function main() {
+  program
+    .option("-s --setup")
+    .option("-b --build")
+    .parse(process.argv);
+  const opts = program.opts();
   try {
-    const files = [
-      "loading.html",
-      "settings.html",
-      "package.json",
-      "images",
-      "locales",
-      "src/renderer/desktop.css"
-    ];
-    for (let file of files) {
-      await fs.copy(file, path.join(__dirname, config.directories.app, file.replace("src/", "")));
+    if (opts.setup) {
+      const copied = await copyFiles();
+      console.log(`Copy ${copied} to dist/*`);
     }
-
-    // issue#49 https://github.com/sakazuki/node-red-desktop/issues/49
-    await fs.copy('patch/i18n.js', path.join(__dirname, config.directories.app, "node_modules/@node-red/util/lib/i18n.js"));
-
-    if (!program.opts().build) return;
-    
-    const platform = (process.platform === "darwin") ? Platform.MAC : Platform.WINDOWS;
-    const res = await builder.build({
-      targets: platform.createTarget(),
-      config
-    });
-    console.log(res);
+    if (opts.build) {
+      await patchFiles();
+      console.log(`Replaced @node-red/util/lib/i18n.js`);
+      const res = await build();
+      console.log(res);
+    }
   }catch(err) {
     console.error(err);
   }
-})();
+}
 
+main();
